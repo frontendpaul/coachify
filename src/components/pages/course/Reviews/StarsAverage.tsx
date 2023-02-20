fef8dda020af189ef885b78279f87414673c2589
frontendpaul <matizbrave@gmail.com>
2023-02-20 12:32:20 +0100

    fix: avg. stars display

--- src/components/pages/course/Reviews/StarsAverage.tsx
+++ src/components/pages/course/Reviews/StarsAverage.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image';
 
 const StarsAverage = ({ rating }: { rating: number }) => {
-  const width = (rating / 5) * 100 + '%';
+  const border = 1;
+  const inner = 18;
+  const space = 4;
+  const fullNumber = Math.floor(rating);
+  const fraction = rating - fullNumber;
+  const width =
+    (border * 2 + inner + space) * fullNumber + fraction * inner + border;
+
+  console.log(width);
+
   return (
     <div className="relative">
-      <Image src="/stars.svg" width={108} height={20} alt="" />
+      <Image src="/stars.svg" width={116} height={20} alt="" />
       <div
         className="absolute top-0 left-0 h-5 bg-white"
         style={{
           mask: 'url(/stars-full.svg)',
           WebkitMask: 'url(/stars-full.svg)',
-          width: width,
+          width: `${width}px`,
         }}
       ></div>
     </div>
